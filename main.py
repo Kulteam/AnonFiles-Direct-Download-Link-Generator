6fd6e4435b2981495a907c8faf1f803628f5ec81
Kulteam <Kulteam@users.noreply.github.com>
2021-12-27 21:06:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 #import urllib2
 import re
 import random
+import wget
 import time
 
 
@@ -14,7 +15,7 @@ file = open("link.txt", "r")
 file_link = file.read()
 links_to_get = file_link.split("\n")
 
-print(links_to_get)
+#print(links_to_get)
 
 for index in range(len(links_to_get)): 
     req = requests.get(links_to_get[index])
@@ -22,12 +23,10 @@ for index in range(len(links_to_get)):
    # print(soup.title)
     for link in soup.findAll('a', attrs={'href': re.compile("^https://cdn-")}):
          link_direct = (link.get('href'))
-         print(link_direct)
-         r = requests.get(link_direct, allow_redirects=True)
-         if link_direct.find('/'):
-             namefile=(link_direct.rsplit('/', 1)[1])
-             open(namefile, 'wb').write(r.content)
-             print("Done downloading: "+namefile)
-             sleep_time=random.randint(5,15)
-             print("Pause until next download for %s s" %sleep_time )
-             time.sleep(sleep_time)
+         print("Download file of link: "+links_to_get[index])
+         filename=wget.download(link_direct)
+         print("Done download filename: "+filename)
+         sleep_time=random.randint(5,15)
+         print("Pause until next download for %s s" %sleep_time )
+         time.sleep(sleep_time)
+         
