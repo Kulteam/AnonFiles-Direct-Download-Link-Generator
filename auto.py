de5e8021702b0d2f04ac171dbf57d98ffe1369a8
Kulteam <Kulteam@users.noreply.github.com>
2021-12-27 08:12:35 +0700

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import random
+import wget
+import time
+
+
+
+list_links=[]
+
+file = open("link.txt", "r")
+
+file_link = file.read()
+links_to_get = file_link.split("\n")
+
+#print(links_to_get)
+
+for index in range(len(links_to_get)): 
+    req = requests.get(links_to_get[index])
+    soup = BeautifulSoup(req.text, "html.parser")
+   # print(soup.title)
+    for link in soup.findAll('a', attrs={'href': re.compile("^https://cdn-")}):
+         link_direct = (link.get('href'))
+         print(link_direct)
+         filename=wget.download(link_direct)
+         print("Done download filename: "+filename)
+         sleep_time=random.randint(5,15)
+         print("Pause until next download for %s s" %sleep_time )
+         time.sleep(sleep_time)
